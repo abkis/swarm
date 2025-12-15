9a523edf65588c0a74260bd277deea967995133b
abkis <kis.anna.borbala@gmail.com>
2025-12-15 12:08:53 -0500

    increase speed

--- src/objects/robots/robot.cpp
+++ src/objects/robots/robot.cpp
@@ -1,5 +1,6 @@
 #include "objects/robots/robot.h"
 #include "helpers.h"
+#include <cmath>
 
 Robot::Robot(float v_x, float v_y, float p_x, float p_y, float size_x, float size_y, float direction, float mass, float max_speed, float speed_incr, int id, sf::Color color) : Object(p_x, p_y, size_x, size_y, color), max_speed{max_speed}, id{id},
                                                                                                                                                                                 speed_incr{speed_incr}, mass{mass}, direction{direction}
@@ -35,4 +36,27 @@ void Robot::on_collision(sf::Vector2f force)
 {
     // changes velocity
     vel = helpers::cap_speed(vel + force, max_speed);
+}
+
+// increase speed in velocity direction
+void Robot::increase_speed()
+{
+    float mag = helpers::mag(vel);
+    if (mag == 0)
+    {
+        // not moving in any direction
+        // use "direction" to describe xy movements
+        // result is unit vector, no extra speed incr needed
+        vel.x = cos(direction);
+        vel.y = sin(direction);
+    }
+    else if (mag > max_speed)
+    {
+        vel = helpers::cap_speed(vel, max_speed); // shouldn't be allowed to go that fast
+    }
+    else if (mag < max_speed)
+    {
+        vel *= speed_incr; // incr by appropriate amount
+        vel = helpers::cap_speed(vel, max_speed);
+    }
 }
